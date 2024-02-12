33a51e5d4b54ca908485db0ac4878d786cf00726
gustavogss <gustavogss.dev@gmail.com>
2024-02-12 17:38:12 -0300

    feat: schema bd configuration

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,31 +1,10 @@
 import fastfi from "fastify";
-import {z} from 'zod';
-import {Poll, PrismaClient} from '@prisma/client';
+import { createPoll } from "../routes/create-poll";
+
 
 const app = fastfi();
 
-const prisma = new PrismaClient();
-
-
-app.get("/", (_req, res) => {
-  return res.send({msg:"Hello World!"});
-});
-
-app.post("/polls", async(req, res) => {
-  const creatPollBody = z.object({
-    title: z.string()
-  })
-  const {title} = creatPollBody.parse(req.body)
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    }
-  })
-
-  return res.status(201).send({pollId: poll.id})
-  
-});
+app.register(createPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server listening on port!");
